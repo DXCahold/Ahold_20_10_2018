59ad708565e4913ca66fd73eec6f55da874735ea
DXCahold <43364687+DXCahold@users.noreply.github.com>
2018-10-22 11:49:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,6 +125,23 @@ def webhook():
 					request_data["result"] = "No such product found! please specify the exact product name"
 			else:
 				request_data["result"] = "Hi! Please share your Phone number for personalized Assistance!"
+		
+		if request_data["unknown"] == "location":
+			if session["signedin"]:
+				detail = {"match":False,"result":""}
+				for sheet in book.keys():
+					for row in book[sheet]:
+						headers = row.keys()
+						if request_data["unknown"] in headers:
+							if request_data["known"]["product"] == row["product"]:
+								detail["match"],detail["result"] = True,row["location"]
+				if 	detail["match"]:
+					request_data["result"] = str(request_data["fulfillmentText"]).replace("*result",detail["result"])
+				else:
+					request_data["result"] = "No such product found! please specify the exact product name"
+				
+			else:
+				request_data["result"] = "Hi! Please share your Phone number for personalized Assistance!"
 		"""
 		else:
 			request_data["result"] = request_data["fulfillmentText"]
