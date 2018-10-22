60cadffb56098c4024b8d8d75f128e65b576694f
DXCahold <43364687+DXCahold@users.noreply.github.com>
2018-10-22 11:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def webhook():
 									if int(float(row["quantity"]))>=int(request_data["known"]["quantity"]):
 										detail["availability"] = "is available"
 									else:
-										detail["availability"] = "sorry! only "str(int(float(row["quantity"])))+" "+request_data["known"]["product"]+" available in stock "
+										detail["availability"] = "sorry! only "+str(int(float(row["quantity"])))+" "+request_data["known"]["product"]+" available in stock "
 								else:
 									detail["availability"] = " will be available on "+str(row["refilldate"])
 						if "offer" in headers:
